5639bcb373254d65b4e75b618298ab433fca574e
Batuhan Inal <np_batu@hotmail.com>
2024-04-17 22:09:30 +0300

    refactor: dependencies

---
+++ src/Infrastructure/Persistence/ServiceRegistration.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Entities.Identities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Contexts;
+
+namespace Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void BindPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<TemplateContext>(options => options.UseNpgsql(configuration.GetConnectionString("PgSQL")));
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequiredLength = 3;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+            });
+        }
+    }
+}
