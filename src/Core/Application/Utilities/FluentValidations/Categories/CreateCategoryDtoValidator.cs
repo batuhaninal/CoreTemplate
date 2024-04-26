6e6f3c59ac52f6cd7636367252329ed2038bee4c
Batuhan Inal <np_batu@hotmail.com>
2024-04-26 19:01:57 +0300

    feat: createcategorydto validator

---
+++ src/Core/Application/Utilities/FluentValidations/Categories/CreateCategoryDtoValidator.cs
@@ -0,0 +1,28 @@
+﻿using Application.Models.DTOs.Categories;
+using Application.Models.Messages;
+using Application.Utilities.Helpers;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Utilities.FluentValidations.Categories
+{
+    public class CreateCategoryDtoValidator : AbstractValidator<CreateCategoryDto>
+    {
+        public CreateCategoryDtoValidator()
+        {
+            RuleFor(x => x.Title)
+                .NotEmpty()
+                    .WithMessage(CommonMessage.Validation.NotNull("Title"))
+                .NotNull()
+                    .WithMessage(CommonMessage.Validation.NotNull("Title"))
+                .MaximumLength(75)
+                    .WithMessage(CommonMessage.Validation.MaxLength("Title", 75))
+                .Must(RegexHelper.CheckWhiteSpaceDuplicate)
+                    .WithMessage(CommonMessage.RegexErr.DuplicateWhiteSpace("Title"));
+        }
+    }
+}
