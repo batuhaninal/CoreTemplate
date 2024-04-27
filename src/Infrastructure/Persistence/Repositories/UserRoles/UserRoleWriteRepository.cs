8d4b08cc9d9cc51583aa080f582c720db2118345
Batuhan Inal <np_batu@hotmail.com>
2024-04-27 23:37:16 +0300

    feat: user,role repository managers added

---
+++ src/Infrastructure/Persistence/Repositories/UserRoles/UserRoleWriteRepository.cs
@@ -0,0 +1,14 @@
+﻿using Application.Abstractions.Repositories.UserRoles;
+using Domain.Entities;
+using Persistence.Contexts;
+using Persistence.Repositories.Commons;
+
+namespace Persistence.Repositories.UserRoles
+{
+    public class UserRoleWriteRepository : WriteRepository<UserRole>, IUserRolesWriteRepository
+    {
+        public UserRoleWriteRepository(TemplateContext context) : base(context)
+        {
+        }
+    }
+}
