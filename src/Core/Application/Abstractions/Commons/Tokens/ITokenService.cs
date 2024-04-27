ec1c94b33be3cf8b7567ada04dc464456ff42291
Batuhan Inal <np_batu@hotmail.com>
2024-04-27 17:12:16 +0300

    feat: token service added

---
+++ src/Core/Application/Abstractions/Commons/Tokens/ITokenService.cs
@@ -0,0 +1,11 @@
+﻿using Application.Models.Tokens;
+using Domain.Entities.Identities;
+
+namespace Application.Abstractions.Commons.Tokens
+{
+    public interface ITokenService
+    {
+        JwtToken CreateAccessToken(User user, int minutes);
+        string CreateRefreshToken();
+    }
+}
